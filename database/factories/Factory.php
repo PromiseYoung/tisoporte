eaaed8ec219da3850795a76ee2e904094ef68b9b
Jaramill0 <hugo98jaramillo@gmail.com>
2024-11-08 13:20:29 -0600

    Se aplica file Factory para la actualizacion de php 8.1

---
+++ database/factories/Factory.php
@@ -0,0 +1,17 @@
+<?php
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UserFactory extends Factory
+{
+    protected $model = User::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => bcrypt('password'),
+        ];
+    }
+}
