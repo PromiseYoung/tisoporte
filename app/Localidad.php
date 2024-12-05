f8b24627807c9ff66ae0f0113de4ae4e090ac585
Jaramill0 <hugo98jaramillo@gmail.com>
2024-12-05 14:49:56 -0600

    Nuevo modelo Localidad ubicacion de soportes brindados en la corporacion

---
+++ app/Localidad.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;  // Asegúrate de usar la ruta correcta de espacio de nombres
+
+use App\Ticket;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Localidad extends Model
+{
+    use HasFactory;
+
+    //  el nombre de la tabla
+    protected $table = 'localidades';
+
+    // No necesitas incluir 'id' ya que Laravel lo maneja automáticamente
+    protected $fillable = ['nombre'];
+
+    /**
+     * Relación de uno a muchos con el modelo Ticket.
+     * Un localidad puede tener muchos tickets.
+     */
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+
+}
