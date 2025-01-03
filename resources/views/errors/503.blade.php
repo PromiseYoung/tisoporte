39d30d56044361313f25670c3a6f0dbb3b0add09
Jaramill0 <hugo98jaramillo@gmail.com>
2025-01-03 11:59:01 -0600

    Implementacion de vista de modo mantenimiento en la App Soporte y Tickets de Servicio

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container mt-5 d-flex justify-content-center align-items-center" style="min-height: 100vh;">
+        <div class="alert alert-danger text-center p-5 rounded shadow-lg w-75">
+            <h1 class="display-4 font-weight-bold">¡Volveremos Pronto!</h1>
+            <p class="lead mb-4">Estamos realizando mantenimiento en el sistema en este momento.</p>
+            <p class="lead mb-4">Volveremos con buenas noticias.</p>
+            <h5 class="font-weight-semibold mb-4">Gracias por tu paciencia. - LOAD TI</h5>
+        </div>
+    </div>
+@endsection
