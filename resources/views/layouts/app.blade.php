2458adec0412b0c3a1fc70018af4fffc7fd3aebf
Jaramill0 <hugo98jaramillo@gmail.com>
2025-02-26 14:03:15 -0600

    se valida mejoras para el adjunto de evidencias

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -250,6 +250,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
         integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous">
     </script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.3/min/dropzone.min.js"></script>
 
 
     @yield('scripts')
