34a52e019a901aa3339f8b8aee3b283bb66c4206
Jaramill0 <hugo98jaramillo@gmail.com>
2025-03-28 08:53:31 -0600

    Refactor main.js: modulariza la inicialización de editores, configuraciones de moment, selectores de fecha y botones de selección

--- public/js/main.js
+++ public/js/main.js
@@ -1,57 +1,69 @@
 $(document).ready(function () {
-    window._token = $('meta[name="csrf-token"]').attr('content')
+    window._token = $('meta[name="csrf-token"]').attr('content');
 
-    var allEditors = document.querySelectorAll('.ckeditor');
-    for (var i = 0; i < allEditors.length; ++i) {
-        ClassicEditor.create(
-            allEditors[i],
-            {
+    initializeEditors();
+    configureMomentLocale();
+    initializeDatePickers();
+    initializeSelect2();
+    handleSelectAllButtons();
+    handleTreeView();
+
+    function initializeEditors() {
+        const allEditors = document.querySelectorAll('.ckeditor');
+        allEditors.forEach(editor => {
+            ClassicEditor.create(editor, {
                 removePlugins: ['ImageUpload']
-            }
-        );
+            });
+        });
     }
 
-    moment.updateLocale('en', {
-        week: { dow: 1 } // Monday is the first day of the week
-    })
+    function configureMomentLocale() {
+        moment.updateLocale('en', {
+            week: { dow: 1 } // Monday is the first day of the week
+        });
+    }
 
-    $('.date').datetimepicker({
-        format: 'YYYY-MM-DD',
-        locale: 'en'
-    })
+    function initializeDatePickers() {
+        $('.date').datetimepicker({
+            format: 'YYYY-MM-DD',
+            locale: 'en'
+        });
 
-    $('.datetime').datetimepicker({
-        format: 'YYYY-MM-DD HH:mm:ss',
-        locale: 'en',
-        sideBySide: true
-    })
+        $('.datetime').datetimepicker({
+            format: 'YYYY-MM-DD HH:mm:ss',
+            locale: 'en',
+            sideBySide: true
+        });
 
-    $('.timepicker').datetimepicker({
-        format: 'HH:mm:ss'
-    })
+        $('.timepicker').datetimepicker({
+            format: 'HH:mm:ss'
+        });
+    }
 
-    $('.select-all').click(function () {
-        let $select2 = $(this).parent().siblings('.select2')
-        $select2.find('option').prop('selected', 'selected')
-        $select2.trigger('change')
-    })
-    $('.deselect-all').click(function () {
-        let $select2 = $(this).parent().siblings('.select2')
-        $select2.find('option').prop('selected', '')
-        $select2.trigger('change')
-    })
+    function initializeSelect2() {
+        $('.select2').select2();
+    }
 
-    $('.select2').select2()
+    function handleSelectAllButtons() {
+        $('.select-all').click(function () {
+            const $select2 = $(this).parent().siblings('.select2');
+            $select2.find('option').prop('selected', 'selected');
+            $select2.trigger('change');
+        });
 
-    $('.treeview').each(function () {
-        var shouldExpand = false
-        $(this).find('li').each(function () {
-            if ($(this).hasClass('active')) {
-                shouldExpand = true
+        $('.deselect-all').click(function () {
+            const $select2 = $(this).parent().siblings('.select2');
+            $select2.find('option').prop('selected', '');
+            $select2.trigger('change');
+        });
+    }
+
+    function handleTreeView() {
+        $('.treeview').each(function () {
+            const shouldExpand = $(this).find('li.active').length > 0;
+            if (shouldExpand) {
+                $(this).addClass('active');
             }
-        })
-        if (shouldExpand) {
-            $(this).addClass('active')
-        }
-    })
-})
+        });
+    }
+});
